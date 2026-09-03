a7a7494a72e2a36a46e2a168f5ba729212c20ce8
Rio <riocandraa99@gmail.com>
2026-09-03 20:52:24 +0800

    Fix commit

---
+++ app/dashboard/[id]/digital-detection/page.tsx
@@ -0,0 +1,7 @@
+export default function DigitalDetectionPage(){
+    return(
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-3xl font-bold text-white">Digital Detection</h1>
+        </div>
+    )
+}
